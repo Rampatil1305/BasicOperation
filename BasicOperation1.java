ea8d939b7f162cda4e1f5e055e693a6d86c022b1
Rampatil1305 <ramanagoudsp13@gmail.com>
2025-05-07 15:08:54 +0530

    master branch

--- BasicOperation1.java
+++ BasicOperation1.java
@@ -5,6 +5,8 @@ public class BasicOperation1{
         System.out.println("Addition of a and b is: "+(a+b));
         System.out.println("Subtraction of a and b is: "+(a-b));
         System.out.println("Multiplication of a and b is: "+(a*b));
+        System.out.println("Division of a and b is: "+(a/b));
+        System.out.println("Modulus of a and b is: "+(a%b));
         
     }
 }
